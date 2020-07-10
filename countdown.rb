d87de62964f0d5bb10feb13af495cf0b0f458192
Nicole Stellatos <stellatosnicole@gmail.com>
2020-07-10 23:21:09 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,6 +1,7 @@
 #write your code here
 def countdown(number_of_seconds)
   while number_of_seconds > 0
-  puts #{number_of_seconds} "SECOND(S)!"
+  puts "#{number_of_seconds} SECOND(S)!"
+  number_of_seconds -=
 end
 end
