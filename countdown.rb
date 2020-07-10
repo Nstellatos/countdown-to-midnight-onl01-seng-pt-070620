627704c9c03069d82be736d045bf9f67059de979
Nicole Stellatos <stellatosnicole@gmail.com>
2020-07-10 23:27:31 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,3 +6,12 @@ def countdown(number_of_seconds)
 end
 "HAPPY NEW YEAR"
 end
+
+def countdown_with_sleep(number_of_seconds)
+  while number_of_seconds > 0
+  puts "#{number_of_seconds} SECOND(S)!"
+  sleep(1)
+  number_of_seconds -= 1
+end
+"HAPPY NEW YEAR"
+end
